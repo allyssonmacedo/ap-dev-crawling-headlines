79f6692c180298436d9a18aeaa74a454aec6524d
Allysson Macedo <allyssonvsmacedo@gmail.com>
2024-06-12 22:27:55 -0300

    fixing crawler class

--- scr/crawler.py
+++ scr/crawler.py
@@ -2,14 +2,19 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-class Crawler(self):
+class Crawler():
+
+    def __init__(self) -> None:
+        pass
 
     def get_news_html(self, search_url: str = 'https://finance.yahoo.com/topic/latest-news/'):
         rq = requests.get(search_url)   
 
         if rq.status_code in range(200, 300, 1):
             print(f'Scrapping de {search_url} finalizado com sucesso')
+            
             return rq
+        
         else:
             raise Exception
         
@@ -29,70 +34,51 @@ class Crawler(self):
 
         return list_items_raw
     
-    def get_news_data(self, html_raw_list: list = get_news_items_html()):
+    def get_news_data(html_raw_list: list = get_news_items_html()):
 
         search_data = [] 
 
         for item in html_raw_list:
 
-            try:
+            # pegando a url
+            url = item.h3.a['href']
 
-                news_data = {
-                'url': 'None',
-                'author': 'None',
-                'headline': 'None',
-                'datetime_news': 'None',
-                'mins_read': 'None',
-                'data_site': 'None',
-                'data_source': 'None',
-                'data_type': 'None',
-                'count_comments': 'None'
-                }
+            # pegando o headline
+            headline = item.h3.text
+
+            # coletando mais informações dentro de cada url
+            news_page = requests.get(url)
+        
+            # fazendo o parse do html
+            news_content = bs(news_page.text, 'html.parser')
+
+            # pegando o autor da notícia
+            author = news_content.select('.caas-attr-item-author')[0].text
+
+            # Datetime e a quantidade de minutos da notícia
+            updated_time = news_content.select('.caas-attr-time-style')
+
+            datetime_news = updated_time[0].time['datetime']
+            mins_read = updated_time[0].select('.caas-attr-mins-read')[0].text.split()[0]
+
+            # pegando outras características da notícia
+            data_site = news_content.select('.reactions-count')[0]['data-site']
+            data_source = news_content.select('.reactions-count')[0]['data-source']
+            data_type = news_content.select('.reactions-count')[0]['data-type']
+            count_comments = news_content.select('.reactions-count')[0].text
             
-                # pegando a url
-                url = item.h3.a['href']
-
-                # pegando o headline
-                headline = item.h3.text
-
-                # coletando mais informações dentro de cada url
-                news_page = requests.get(url)
-            
-                # fazendo o parse do html
-                news_content = bs(news_page.text, 'html.parser')
-
-                # pegando o autor da notícia
-                author = news_content.select('.caas-attr-item-author')[0].text
-
-                # Datetime e a quantidade de minutos da notícia
-                updated_time = news_content.select('.caas-attr-time-style')
-
-                datetime_news = updated_time[0].time['datetime']
-                mins_read = updated_time[0].select('.caas-attr-mins-read')[0].text.split()[0]
-
-                # pegando outras características da notícia
-                data_site = news_content.select('.reactions-count')[0]['data-site']
-                data_source = news_content.select('.reactions-count')[0]['data-source']
-                data_type = news_content.select('.reactions-count')[0]['data-type']
-                count_comments = news_content.select('.reactions-count')[0].text
-            
-            except: 
-                pass
-
             news_data = {
-                'url': url,
-                'author': author,
-                'headline': headline,
-                'datetime_news': datetime_news,
-                'mins_read': mins_read,
-                'data_site': data_site,
-                'data_source': data_source,
-                'data_type': data_type,
-                'count_comments': count_comments
+                'url': url if len(url) > 0 else 'None',
+                'author': author if len(author) > 0  else 'None',
+                'headline': headline if len(headline) > 0 else 'None',
+                'datetime_news': datetime_news if len(datetime_news) > 0 else 'None',
+                'mins_read': mins_read if len(mins_read) > 0 else 'None',
+                'data_site': data_site if len(data_site) > 0 else 'None',
+                'data_source': data_source if len(data_source) > 0 else 'None',
+                'data_type': data_type if len(data_type) > 0 else 'None',
+                'count_comments': count_comments if len(count_comments) > 0 else 'None'
             }
         
             search_data.append(news_data)
 
         return search_data
-            
-        
