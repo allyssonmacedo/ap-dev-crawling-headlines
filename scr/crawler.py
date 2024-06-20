8b69c6a52a08d9e5a3fd71ac0c679ced2d4581b6
Allysson Macedo <allyssonvsmacedo@gmail.com>
2024-06-19 21:19:46 -0300

    Doc: Adding the docstrings in the crawler functions

--- scr/crawler.py
+++ scr/crawler.py
@@ -8,6 +8,18 @@ class Crawler():
         pass
 
     def get_news_html(self, search_url: str = 'https://finance.yahoo.com/topic/latest-news/'):
+        """This function verify the request status and returns the raw html researched when the request code is between (200,300)
+
+        Args:
+            search_url (string, optional): A string that contains a url link that leads to the website to scrape. Only the default link has been tested.
+            Defaults to 'https://finance.yahoo.com/topic/latest-news/'.
+
+        Raises:
+            Exception: A error message
+
+        Returns:
+            The raw html scrapped webpage
+        """
         rq = requests.get(search_url)   
 
         if rq.status_code in range(200, 300, 1):
@@ -19,6 +31,14 @@ class Crawler():
             raise Exception("url inválido") 
     
     def get_news_soup(self, raw_html = None):
+        """This function gets the raw html and return a parsed html that has been researched
+
+        Args:
+            raw_html (get_news_html(), optional): a raw html page. When the default is set (None) it will automatically performs the get_news_html(). Defaults to None.
+
+        Returns:
+            html: A parsed html
+        """
         if raw_html == None:
             raw_html = self.get_news_html()
 
@@ -26,6 +46,15 @@ class Crawler():
         return soup
 
     def get_news_items_html(self, soup=None, max_index=22):
+        """This function gets the html for each item in the main researched page
+
+        Args:
+            soup (get_news_soup(), optional): a parsed html that will be filter to get the items html. Defaults to None.
+            max_index (int, optional): the number of max items to return in the scrapping. Due the specifics of the page (currently), we can't perform more than 22 items each time. Defaults to 22.
+
+        Returns:
+            list: A list that contains the html of interest for each item in the main research
+        """
 
         if soup == None: 
             soup = self.get_news_soup()
@@ -42,12 +71,29 @@ class Crawler():
         return list_items_raw
     
     def valid_info(self, field):
+        """This function has the intent to verify the info that has been scrapped. 
+           It only verifies if the field exists, if so, it will be collect, otherwise 'None' is returned
+
+        Args:
+            field (css element): the css element that will be catch in the scrapping
+
+        Returns:
+            It will return the element itself if exists, otherwise, it will return 'None' string
+        """
         try:
             return field
         except:
             return 'None'
     
     def get_news_data(self, html_raw_list: None):
+        """This functions gets the data for each item in the list of items collected. 
+
+        Args:
+            html_raw_list (get_news_items_html()): A list that was build before in the previously get_news_items_html() function. The code
+
+        Returns:
+            list: A list of dictionaries that contains the information for each item scrapped.
+        """
 
         if html_raw_list == None:
             html_raw_list = self.get_news_items_html()
